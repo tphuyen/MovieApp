27609917021f76f9712bafdfb7287de79b3e6231
tphuyen <trinhhuyen.tph@gmail.com>
2025-02-12 16:45:27 +0700

    Done build flavors

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,32 @@ import 'package:movie_app/viewmodel/save_movie_view_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  const String flavor = String.fromEnvironment('FLAVOR', defaultValue: 'dev');
+
+  runApp(MyApp(flavor: flavor));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String flavor;
+
+  const MyApp({Key? key, required this.flavor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => BaseViewModel()),
-          ChangeNotifierProvider(create: (_) => HomeViewModel()),
-          ChangeNotifierProvider(create: (context) => MovieViewModel()),
-          ChangeNotifierProvider(create: (_) => SavedMovieViewModel()),
-        ],
-        child: const MaterialApp(
-          title: 'Movie App',
-          debugShowCheckedModeBanner: false,
-          initialRoute: RoutesNames.movies,
-          onGenerateRoute: AppRoutes.generateRoute,
-          home: CustomBottomNavBar(),
-        ));
+      providers: [
+        ChangeNotifierProvider(create: (_) => BaseViewModel()),
+        ChangeNotifierProvider(create: (_) => HomeViewModel()),
+        ChangeNotifierProvider(create: (context) => MovieViewModel()),
+        ChangeNotifierProvider(create: (_) => SavedMovieViewModel()),
+      ],
+      child: MaterialApp(
+        title: flavor == "dev" ? "Movie App (Dev)" : "Movie App",
+        debugShowCheckedModeBanner: false,
+        initialRoute: RoutesNames.movies,
+        onGenerateRoute: AppRoutes.generateRoute,
+        home: CustomBottomNavBar(),
+      ),
+    );
   }
 }
---
+++ lib/main_prod.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/main.dart';
+
+void main() {
+  runApp(const MyApp(flavor: 'prod'));
+}
