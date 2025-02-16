ad5eebcc2b44e52ce9013fcb9144e3d4d0fa39ec
tphuyen <trinhhuyen.tph@gmail.com>
2025-02-16 15:14:26 +0700

    Done add fcm

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_app/services/notification_service.dart';
 import 'package:movie_app/utils/bottom_navigation/custom_bottom_navbar.dart';
 import 'package:movie_app/utils/routes/routes.dart';
 import 'package:movie_app/utils/routes/routes_names.dart';
@@ -7,15 +9,27 @@ import 'package:movie_app/viewmodel/home_view_model.dart';
 import 'package:movie_app/viewmodel/movie_view_model.dart';
 import 'package:movie_app/viewmodel/save_movie_view_model.dart';
 import 'package:provider/provider.dart';
-
 import 'package:movie_app/environment/flavor.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
 
 late Flavor flavor;
 
-class MyApp extends StatelessWidget {
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await NotificationService.instance.initialize();
+  runApp(const MyApp());
+}
 
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,8 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/environment/flavor.dart';
 import 'package:movie_app/main.dart';
 
-void main() {
+import 'package:movie_app/firebase_options_prod.dart';
+import 'package:movie_app/services/notification_service.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );  // Khởi tạo Firebase
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print("FCM token: ${fcmToken}");
+  await NotificationService.instance.initialize();
   flavor = Flavor.prod;
   runApp(const MyApp());
 }
